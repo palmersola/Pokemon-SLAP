6193a2795c770e57b242292fa3c4f09a299c0217
KappaMustafa <steef136@gmail.com>
2022-08-05 14:47:51 -0500

    modified a pokemon route

--- controllers/pokemon_routes.js
+++ controllers/pokemon_routes.js
@@ -10,19 +10,4 @@ pokemon_routes.get('/', (req, res) => {
     });
   });
 
-
-pokemon_routes.post('/:id', (req, res) =>{
-    Pokemon.create({
-        pokemon_name: req.body.pokemon_name,
-        experience: req.body.experience,
-        attack_stat: req.body.attack_stat,
-        defense_stat: req.body.defense_stat,
-        speed_stat: req.body.speed_stat,
-        sprite: req.body.sprite,
-    }).then(new_pokemon =>{
-        res.send(new_pokemon)
-    })
-})
-
-Sprites.versions.generation-V.black-white.animated.front_default
 module.exports = pokemon_routes
