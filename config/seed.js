d36baf4234923de6960de8e5a8a17a0c85c689dc
KappaMustafa <steef136@gmail.com>
2022-08-08 15:21:16 -0500

    committing again

--- config/seed.js
+++ config/seed.js
@@ -25,52 +25,25 @@ async function pokeApiCaller(id){
 }
 pokePuller();
 
-// User.bulkCreate([
-//   {
-//     user_name: "Derek",
-//     password: "testing"
-//   },
-//   {
-//     user_name: "Mustafa",
-//     password: "testing"
-//   },
-//   {
-//     user_name: "Andy",
-//     password: "testing"
-//   },
-//   {
-//     user_name: "Palmer",
-//     password: "testing"
-//   }
-// ]);
+User.bulkCreate([
+  {
+    user_name: "Derek",
+    password: "testing"
+  },
+  {
+    user_name: "Mustafa",
+    password: "testing"
+  },
+  {
+    user_name: "Andy",
+    password: "testing"
+  },
+  {
+    user_name: "Palmer",
+    password: "testing"
+  }
+]);
 
-<<<<<<< HEAD
-    {
-      pokemon_name: "Rattata",
-      experience: 100,
-      attack_stat: 50,
-      defense_stat: 50,
-      speed_stat: 50,
-      sprite: "test"
-    },
-    {
-      pokemon_name: "Ekans",
-      experience: 100,
-      attack_stat: 50,
-      defense_stat: 50,
-      speed_stat: 50,
-      sprite: "test"
-    },
-    {
-      pokemon_name: "Sandshrew",
-      experience: 100,
-      attack_stat: 50,
-      defense_stat: 50,
-      speed_stat: 50,
-      sprite: "test"
-    }
-  ]);
-});
 Character.bulkCreate([
   {
     level: 1,
@@ -103,7 +76,7 @@ Character.bulkCreate([
     speed_stat: 30,
     sprite: "test",
     user_id: 4
-  }
+  },
 
 ]);
 
@@ -197,134 +170,4 @@ Pokemon.bulkCreate([
     sprite: "test"
   }
 ]);
-=======
-// Character.bulkCreate([
-//   {
-//     level: 1,
-//     hp_stat: 100,
-//     attack_stat: 40,
-//     defense_stat: 60,
-//     speed_stat: 70,
-//     sprite: "test",
-//     user_id: 1
-//   },
-//   {
-//     level: 1,
-//     hp_stat: 100,
-//     attack_stat: 70,
-//     defense_stat: 50,
-//     speed_stat: 40,
-//     sprite: "test",
-//     user_id: 2
-//   },
-//   {
-//     level: 1,
-//     hp_stat: 100,
-//     attack_stat: 80,
-//     defense_stat: 30,
-//     speed_stat: 40,
-//     sprite: "test",
-//     user_id: 3
-//   },
-//   {
-//     level: 1,
-//     hp_stat: 100,
-//     attack_stat: 40,
-//     defense_stat: 80,
-//     speed_stat: 30,
-//     sprite: "test",
-//     user_id: 4
-//   }
-// ]);
->>>>>>> 2a75c17c29ab21e1eabb5bc7d8e0c25aba6b4902
 
-// Pokemon.bulkCreate([
-//   {
-//     pokemon_name: "Bulbasaur",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Charmander",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Squirtle",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Jigglypuff",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Celfairy",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Pikachu",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Weedle",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Caterpie",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Rattata",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Ekans",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   },
-//   {
-//     pokemon_name: "Sandshrew",
-//     experience: 100,
-//     attack_stat: 50,
-//     defense_stat: 50,
-//     speed_stat: 50,
-//     sprite: "test"
-//   }
-// ]);
