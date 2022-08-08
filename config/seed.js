cb5b997b52b63b5451a71d81df524a3e86f380e4
savoryboi <andbjerk@yahoo.com>
2022-08-08 10:27:57 -0500

    minor hbs changes

--- config/seed.js
+++ config/seed.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const router = require('express').Router();
 const Character = require('../models/Character');
 const Pokemon = require('../models/Pokemon');
@@ -58,64 +57,6 @@ Character.bulkCreate([
         sprite: 'test',
         user_id: 4,
     },
-=======
-const Character = require("../models/Character");
-const Pokemon = require("../models/Pokemon");
-const User = require("../models/User");
-
-User.bulkCreate([
-  {
-    user_name: "Derek",
-    password: "testing"
-  },
-  {
-    user_name: "Mustafa",
-    password: "testing"
-  },
-  {
-    user_name: "Andy",
-    password: "testing"
-  },
-  {
-    user_name: "Palmer",
-    password: "testing"
-  }
-]);
-
-Character.bulkCreate([
-  {
-    level: 1,
-    attack_stat: 40,
-    defense_stat: 60,
-    speed_stat: 70,
-    sprite: "test",
-    user_id: 1
-  },
-  {
-    level: 1,
-    attack_stat: 70,
-    defense_stat: 50,
-    speed_stat: 40,
-    sprite: "test",
-    user_id: 2
-  },
-  {
-    level: 1,
-    attack_stat: 80,
-    defense_stat: 30,
-    speed_stat: 40,
-    sprite: "test",
-    user_id: 3
-  },
-  {
-    level: 1,
-    attack_stat: 40,
-    defense_stat: 80,
-    speed_stat: 30,
-    sprite: "test",
-    user_id: 4
-  }
->>>>>>> Stashed changes
 ]);
 
 Pokemon.bulkCreate([
