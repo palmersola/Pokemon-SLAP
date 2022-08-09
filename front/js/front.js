3dfe93a60fc8d14b504e7835e414ea3c2acabf66
palmersola <palmersola@yahoo.com>
2022-08-09 12:45:49 -0500

    working on game

--- front/js/front.js
+++ front/js/front.js
@@ -1,46 +1,46 @@
-// let plyHp = "{{player.dataValues.hp_stat}}";
-// let plyAtk = "{{player.dataValues.attack_stat}}";
-// let plyDef = "{{player.dataValues.defense_stat}}";
-// let plyLevel = "{{player.dataValues.level}}";
-// let oppHp = "{{opponent.dataValues.hp_stat}}";
-// let oppAtk = "{{opponent.dataValues.attack_stat}}";
-// let oppDef = "{{opponent.dataValues.defense_stat}}";
-// let oppLevel = "{{opponent.dataValues.level}}";
-// parseInt()
-
-console.log(oppHp);
 document.addEventListener("keyup", event => {
   if (event.code === "Space") {
     console.log("pokemon slapped");
-    oppHp = playerSlap();
+    turn();
   }
 });
 
-function playerSlap() {
+async function turn() {
+  if (plySpd > oppSpd) {
+    oppHp = playerSlap();
+    setTimeout(() => {
+      plyHp = opponentSlap();
+    }, 1500);
+  } else {
+    plyHp = await opponentSlap();
+    oppHp = await playerSlap();
+  }
+}
+
+async function playerSlap() {
   let dmg = Math.floor(
     (2 * plyLevel / 5 + 2) * 100 * (plyAtk / oppDef) / 50 + 2
   );
-  console.log(dmg);
+  console.log("player damge" + dmg);
   let newHp = oppHp - dmg;
-  console.log(newHp);
+  console.log("opponent health" + newHp);
   document.getElementById("oppHp").innerText = newHp;
+  setTimeout(() => {
+    document.getElementById("oppSprt").setAttribute("class", "animation");
+  }, 400);
+  document.getElementById("oppSprt").removeAttribute("class", "animation");
+
   return newHp;
 }
-// function opponentSlap() {
-//   let dmg = Math.floor(
-//     (2 * plyLevel / 5 + 2) * 100 * (plyAtk / oppDef) / 50 + 2
-//   );
-//   console.log(dmg);
-//   let newHp = oppHp - dmg;
-//   console.log(newHp);
-//   document.getElementById("oppHp").innerText = newHp;
-//   return newHp;
-// }
-
-function winner() {}
-function levelUp() {
-  // will increase stats and update them in db to current stats of the lvl
-}
-function loser() {
-  // saves score and adds it to leaderboard and resets the game to start
+async function opponentSlap() {
+  let dmg = Math.floor((2 * 1 / 5 + 2) * 100 * (oppAtk / plyDef) / 50 + 2);
+  console.log(dmg);
+  let newHp = plyHp - dmg;
+  console.log(newHp);
+  document.getElementById("plyHp").innerText = newHp;
+  setTimeout(() => {
+    document.getElementById("plySprt").setAttribute("class", "animation");
+  }, 400);
+  document.getElementById("plySprt").removeAttribute("class", "animation");
+  return newHp;
 }
