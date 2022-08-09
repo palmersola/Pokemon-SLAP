b0359373277c2badcfe188e62a17ab9056b4b4ea
palmersola <palmersola@yahoo.com>
2022-08-09 12:02:48 -0500

    basic game

--- controllers/pokemon_routes.js
+++ controllers/pokemon_routes.js
@@ -1,14 +1,19 @@
-const pokemon_routes = require('express').Router(); 
-const { User, Pokemon, Character } = require('../models');
-console.log('loaded pokemon_routes');
+const pokemon_routes = require("express").Router();
+const { User, Pokemon, Character } = require("../models");
+console.log("loaded pokemon_routes");
 
-pokemon_routes.get('/play', async (req, res) => {
-    const playerName = await User.findByPk(req.session.user_id);
-    const player = await Character.findByPk(req.session.user_id);
-    const opponent = await Pokemon.findByPk(player.dataValues.level);
-    console.log(opponent);
-    console.log(player);
-    res.render("play", { player, opponent, playerName })
+pokemon_routes.get("/play", async (req, res) => {
+  const playerName = await User.findByPk(req.session.user_id);
+  const player = await Character.findByPk(req.session.user_id);
+  const opponent = await Pokemon.findByPk(player.dataValues.level);
+  console.log(opponent);
+  console.log(player);
+  res.render("play", {
+    player,
+    opponent,
+    playerName,
+    something: "something from the backend"
   });
+});
 
-module.exports = pokemon_routes
+module.exports = pokemon_routes;
