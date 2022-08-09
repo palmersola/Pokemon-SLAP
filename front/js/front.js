a766a9fad6c2bf7e27ddad25c990f19a95e316ba
KappaMustafa <steef136@gmail.com>
2022-08-09 12:47:35 -0500

    more func stuff

--- front/js/front.js
+++ front/js/front.js
@@ -1,13 +1,3 @@
-// let plyHp = "{{player.dataValues.hp_stat}}";
-// let plyAtk = "{{player.dataValues.attack_stat}}";
-// let plyDef = "{{player.dataValues.defense_stat}}";
-// let plyLevel = "{{player.dataValues.level}}";
-// let oppHp = "{{opponent.dataValues.hp_stat}}";
-// let oppAtk = "{{opponent.dataValues.attack_stat}}";
-// let oppDef = "{{opponent.dataValues.defense_stat}}";
-// let oppLevel = "{{opponent.dataValues.level}}";
-// parseInt()
-
 console.log(oppHp);
 document.addEventListener("keyup", event => {
   if (event.code === "Space") {
@@ -24,23 +14,40 @@ function playerSlap() {
   let newHp = oppHp - dmg;
   console.log(newHp);
   document.getElementById("oppHp").innerText = newHp;
+  if(oppHp <= 0){
+    pokeKO();
+} else if(plyHp <= 0){
+    plyKO();
+}
   return newHp;
 }
-// function opponentSlap() {
-//   let dmg = Math.floor(
-//     (2 * plyLevel / 5 + 2) * 100 * (plyAtk / oppDef) / 50 + 2
-//   );
-//   console.log(dmg);
-//   let newHp = oppHp - dmg;
-//   console.log(newHp);
-//   document.getElementById("oppHp").innerText = newHp;
-//   return newHp;
-// }
 
-function winner() {}
-function levelUp() {
+let round = 1
+function pokeKO(){
+    console.log("you KO'D pokemon");
+    round ++
+    levelUp(round)
+}
+function plyKO(){
+    console.log("you were KO'D by pokemon");
+    loser();
+}
+
+function levelUp(round) {
   // will increase stats and update them in db to current stats of the lvl
+  let plyLevel = round
+  let updatedPlyHp = Math.floor((0.01 *(2 * plyHp * plyLevel) + plyLevel + 10))
+  let updatedPlyAtk = Math.floor((0.01 *(2 * plyAtk * plyLevel) + 5))
+  let updatedPlyLevel = plyLevel + 1
+  let updatedPlyDef = Math.floor((0.01 *(2 * plyDef * plyLevel) + 5))
+  document.getElementById("plyHp").innerText = updatedPlyHp;
 }
 function loser() {
   // saves score and adds it to leaderboard and resets the game to start
+  let updatedPlyHp = plyHp
+  let updatedPlyAtk = plyAtk
+  let updatedPlyLevel = plyLevel 
+  let updatedPlyDef = plyDef
 }
+
+
