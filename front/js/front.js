f9ec421d789486721c9742d274b507648b63017a
Derjbass <Derek.j.bass@gmail.com>
2022-08-09 16:02:21 -0500

    for main merge

--- front/js/front.js
+++ front/js/front.js
@@ -1,61 +1,73 @@
-
-
-
+let slapping = false
+const basePlyHp = plyHp;
+const basePlyAtk = plyAtk;
+const basePlyDef = plyDef;
+const basePlySpd = plySpd;
 document.addEventListener("keyup", event => {
   if (event.code === "Space") {
     console.log("pokemon slapped");
+    event.preventDefault();
     turn();
   }
 });
+let newHp;
 
 async function turn() {
+  if (slapping) return;
+  slapping = true
   if (plySpd > oppSpd) {
-    oppHp = playerSlap();
-    setTimeout(() => {
-      plyHp = opponentSlap();
-    }, 1500);
+    oppHp = await playerSlap();
+    setTimeout(async () => {
+      plyHp = await opponentSlap();
+      slapping = false;
+      await koCheck();
+    }, 500);
   } else {
     plyHp = await opponentSlap();
-    oppHp = await playerSlap();
+    setTimeout(async () => {
+      oppHp = await playerSlap();
+      slapping = false;
+      await koCheck();
+    }, 500);
   }
+  
 }
 
 async function playerSlap() {
   let dmg = Math.floor(
     (2 * plyLevel / 5 + 2) * 100 * (plyAtk / oppDef) / 50 + 2
   );
-  console.log("player damge" + dmg);
   let newHp = oppHp - dmg;
-  console.log("opponent health" + newHp);
+console.log(dmg);
   document.getElementById("oppHp").innerText = newHp;
-  if(oppHp <= 0){
-    pokeKO();
-    return;
-    }  
-  setTimeout(() => {
+ 
     document.getElementById("oppSprt").setAttribute("class", "animation");
+  setTimeout(() => {
+    document.getElementById("oppSprt").classList.remove("animation");
   }, 400);
-  document.getElementById("oppSprt").removeAttribute("class", "animation");
   return newHp;
 }
 
 
 async function opponentSlap() {
     let dmg = Math.floor((2 * 1 / 5 + 2) * 100 * (oppAtk / plyDef) / 50 + 2);
-    console.log(dmg);
     let newHp = plyHp - dmg;
-    console.log(newHp);
     document.getElementById("plyHp").innerText = newHp;
-    if(plyHp <= 0){
-        plyKO();
-        return;
-    }
-    setTimeout(() => {
-      document.getElementById("plySprt").setAttribute("class", "animation");
-    }, 400);
-    document.getElementById("plySprt").removeAttribute("class", "animation");
 
+    document.getElementById("plySprt").setAttribute("class", "animation");
+    setTimeout(() => {
+      document.getElementById("plySprt").classList.remove("animation");
+    }, 400);
+    
     return newHp;  
+}
+
+async function koCheck(){
+  if(oppHp < 1){
+    return pokeKO();
+  }else if(plyHp < 1){
+    return plyKO();
+  }
 }
 
 let round = 1
@@ -69,14 +81,27 @@ function plyKO(){
     loser();
 }
 
-function levelUp(round) {
+async function levelUp(round) {
+  console.log("level up called");
   // will increase stats and update them in db to current stats of the lvl
   let plyLevel = round
-  let updatedPlyHp = Math.floor((0.01 *(2 * plyHp * plyLevel) + plyLevel + 10))
-  let updatedPlyAtk = Math.floor((0.01 *(2 * plyAtk * plyLevel) + 5))
-  let updatedPlyLevel = plyLevel + 1
-  let updatedPlyDef = Math.floor((0.01 *(2 * plyDef * plyLevel) + 5))
-  document.getElementById("plyHp").innerText = updatedPlyHp;
+  plyHp = Math.floor((0.01 *(2 * basePlyHp * plyLevel) + plyLevel + 10) + basePlyHp)
+  plyAtk = Math.floor((0.01 *(2 * basePlyAtk * plyLevel) + 5) + basePlyAtk)
+  plyDef = Math.floor((0.01 *(2 * basePlyDef * plyLevel) + 5) + basePlyDef)
+  plySpd = Math.floor((0.01 *(2 * basePlySpd * plyLevel) + 5) + basePlySpd)
+  document.getElementById("plyHp").innerText = plyHp;
+  let nextPoke = await axios.get(`/play/${plyLevel}`);
+  console.log(nextPoke);
+  //update next pokemon stats based off level
+  oppHP = Math.floor((0.01 *(2 * nextPoke.data.hp_stat * plyLevel) + plyLevel + 10) + nextPoke.data.hp_stat);
+  oppAtk = Math.floor((0.01 *(2 * nextPoke.data.attack_stat * plyLevel) + 5) + nextPoke.data.attack_stat);
+  oppDef = Math.floor((0.01 *(2 * nextPoke.data.defense_stat * plyLevel) + 5) + nextPoke.data.defense_stat);
+  oppSpd = Math.floor((0.01 *(2 * nextPoke.data.speed_stat * plyLevel) + 5) + nextPoke.data.speed_stat);
+  //update image source
+  console.log(`HP: ${oppHP} Atk: ${oppAtk} Def: ${oppDef} Spd: ${oppSpd}`);
+  document.getElementById("oppSprt").src= nextPoke.data.sprite;
+  document.getElementById("oppName").innerText = nextPoke.data.pokemon_name;
+  document.getElementById("oppHp").innerText = oppHP;
 }
 function loser() {
   // saves score and adds it to leaderboard and resets the game to start
