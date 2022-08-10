930e51d76ce5224efaaf29aca004e4352275b712
Derjbass <Derek.j.bass@gmail.com>
2022-08-09 21:23:54 -0500

    for merge

--- front/js/front.js
+++ front/js/front.js
@@ -16,6 +16,11 @@ document.addEventListener("keyup", event => {
     turn();
   }
 });
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode === 32 && e.target === document.body) {
+    e.preventDefault();
+  }
+});
 
 
 
